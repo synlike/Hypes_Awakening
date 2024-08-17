f984aa04dcea2b7a0254bcd52ffd31c57fb052b5
David <synlike@gmail.com>
2024-08-17 22:50:33 +0200

    Adding Generic State Machine

---
+++ Assets/--- GAME ---/Scripts/StateMachine/StateManager.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StateManager<EState> : MonoBehaviour where EState : Enum
+{
+    protected Dictionary<EState, BaseState<EState>> States = new Dictionary<EState, BaseState<EState>>();
+    protected BaseState<EState> CurrentState;
+
+    protected bool IsTransitioningState = false;
+
+    void Start()
+    {
+        CurrentState.EnterState();
+    }
+
+    void Update()
+    {
+        EState nextStateKey = CurrentState.GetNextState();
+
+        if(!IsTransitioningState && nextStateKey.Equals(CurrentState.StateKey))
+        {
+            CurrentState.UpdateState();
+        }
+        else if (!IsTransitioningState)
+        {
+            TransitionToState(nextStateKey);
+        }
+    }
+
+    public void TransitionToState(EState stateKey)
+    {
+        IsTransitioningState = true;
+
+        CurrentState.ExitState();
+        CurrentState = States[stateKey];
+        CurrentState.EnterState();
+
+        IsTransitioningState = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        CurrentState.OnTriggerEnter(other);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        CurrentState.OnTriggerStay(other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        CurrentState.OnTriggerExit(other);
+    }
+}
